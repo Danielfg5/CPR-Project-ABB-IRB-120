2063ebd9dc01a563d71ccf3f37146171e0c9f692
speiss <107415548+speiss@users.noreply.github.com>
2023-12-19 04:14:29 +0100

    Added motion planning with moveit and my_pid_6dof controller.

--- CPR_project/my_pid_6dof/src/my_pid_6dof.cpp
+++ CPR_project/my_pid_6dof/src/my_pid_6dof.cpp
@@ -6,17 +6,35 @@
 #include <time.h>
 #include <string>
 
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <geometry_msgs/PoseStamped.h>
+
+#include "actionlib/server/simple_action_server.h"
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <control_msgs/JointTrajectoryAction.h>
+
+#include <trajectory_interface/trajectory_interface.h>
+
+#include <joint_trajectory_controller/joint_trajectory_segment.h>
+#include <joint_trajectory_controller/init_joint_trajectory.h>
+#include <joint_trajectory_controller/hardware_interface_adapter.h>
+
 #define N_DOF   6
 
 namespace my_pid_6dof_ns {
 
     class MyPid6Dof : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
 
-        bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n) {
-            
-            std::vector<std::string> joint_names_;
+        typedef actionlib::ActionServer<control_msgs::FollowJointTrajectoryAction>                  ActionServer;
+        typedef boost::shared_ptr<ActionServer>                                                     ActionServerPtr;
+        typedef ActionServer::GoalHandle                                                            GoalHandle;
 
-            /* --- Load parameters from the parameter server (set through joints_controllers.yaml) --- */
+        ActionServerPtr    action_server_;
+
+        bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n) {
+
+            /* --- Load parameters from the parameter server --- */
             if(!n.getParam("joints", joint_names_)){
                 ROS_ERROR("Failed to get joints parameter.");
                 return false;
@@ -45,16 +63,24 @@ namespace my_pid_6dof_ns {
                 }
 
                 /* --- Initialize PID controllers --- */
-                pidControllers_[i].initPid(pid_params_[i].p, pid_params_[i].i, pid_params_[i].d, 100.0, -100.0);
+                pidControllers_[i].initPid(pid_params_[i].p, pid_params_[i].i, pid_params_[i].d, 100.0, -100.0); // TODO: integrator limits, anti-windup
+                // pidControllers_[i].initPid(pid_params_[i].p, 0.0, 0.0, 100.0, -100.0);  // DEBUG
             }
 
-            /* --- Initialize subscribers for desired positions --- */
+            /* --- Initialize subscribers for desired positions (for manual control) --- */
             sub_command1_ = n.subscribe<std_msgs::Float64>("command1", 1, &MyPid6Dof::setCommand1CB, this);
             sub_command2_ = n.subscribe<std_msgs::Float64>("command2", 1, &MyPid6Dof::setCommand2CB, this);
             sub_command3_ = n.subscribe<std_msgs::Float64>("command3", 1, &MyPid6Dof::setCommand3CB, this);
             sub_command4_ = n.subscribe<std_msgs::Float64>("command4", 1, &MyPid6Dof::setCommand4CB, this);
             sub_command5_ = n.subscribe<std_msgs::Float64>("command5", 1, &MyPid6Dof::setCommand5CB, this);
             sub_command6_ = n.subscribe<std_msgs::Float64>("command6", 1, &MyPid6Dof::setCommand6CB, this);
+
+            /* --- Initialize action server --- */
+            action_server_.reset(new ActionServer(n, "/arm_group_controller/follow_joint_trajectory", 
+                                    boost::bind(&MyPid6Dof::goalCB,   this, _1),
+                                    boost::bind(&MyPid6Dof::cancelCB, this, _1), false));
+
+            action_server_->start();
 
             return true;
         }
@@ -68,7 +94,7 @@ namespace my_pid_6dof_ns {
             }
         }
 
-        /* --- Callback functions for desired position subscribers --- */
+        /* --- Callback functions for desired position subscribers (for manual control) --- */
         void setCommand1CB(const std_msgs::Float64ConstPtr& msg){
 
             commands_[0] = msg->data;
@@ -94,6 +120,34 @@ namespace my_pid_6dof_ns {
             commands_[5] = msg->data;
         }
 
+        void goalCB(GoalHandle gh)
+        {
+            // Cancel current goal (if active)
+            if (has_active_goal_){
+                active_goal_.setCanceled();
+            }
+
+            gh.setAccepted();
+            active_goal_ = gh;
+            has_active_goal_ = true;
+
+            trajectory_msgs::JointTrajectory traj = gh.getGoal()->trajectory;
+
+            int n = traj.points.size();            
+            for (int i=0; i<6; i++){                
+                commands_[i] = traj.points[n-1].positions[i];
+            }
+
+        }
+
+        void cancelCB(GoalHandle gh)
+        {
+            if (active_goal_ == gh){
+                active_goal_.setCanceled();
+                has_active_goal_ = false;
+            }
+        }
+
         void starting(const ros::Time& time) {}
         void stopping(const ros::Time& time) {}
 
@@ -115,6 +169,11 @@ namespace my_pid_6dof_ns {
                 double i;
                 double d;
             } pid_params_[N_DOF];
+
+            std::vector<std::string> joint_names_;
+
+            bool has_active_goal_ = false;
+            GoalHandle active_goal_;
     };
 
     PLUGINLIB_EXPORT_CLASS(my_pid_6dof_ns::MyPid6Dof, controller_interface::ControllerBase);
--- README.md
+++ README.md
@@ -8,11 +8,22 @@ Copy folder CPR_project into {ros_workspace}/src.
 Then, inside {ros_workspace}, launch:  
     catkin_make
 
-## How to launch simulation:
+## How to launch simulation (with moveit motion planning):
+
+	roslaunch irb120_moveit_config gazebo.launch
+	roslaunch irb120_moveit_config moveit_rviz.launch
+The parameters for the controller in this simulation are located in "irb120_moveit_config/config/ros_controllers.yaml"
+The joints can also be manually controlled by executing:  
+	rostopic pub -1 /arm_group_controller/commandX std_msgs/Float64 "data: Y"
+
+substituting X with the joint number (1-6) and Y with the joint position (rad)
+
+## How to launch simulation (manual control of individual joints only, no motion planning):
 
 	roslaunch irb120 irb120.launch
+The parameters for the controller in this simulation are located in "irb120/config/joints_controllers.yaml"  
 
-## How to set desired joint positions:
+The joints can be manually controlled by executing:  
 
 	rostopic pub -1 /irb120/joints_positions_controller/commandX std_msgs/Float64 "data: Y"
 
